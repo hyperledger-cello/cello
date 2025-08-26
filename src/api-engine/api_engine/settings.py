53f99296ed9f6a07e15c9f53962b0401952edf82
dodo920306 <dodo920306@gmail.com>
2025-08-26 14:47:26 +0800

    Create 'auth' application
    Signed-off-by: KIRIN.CHU 朱祐麟 <KIRIN.CHU@deltaww.com>
    Signed-off-by: dodo920306 <dodo920306@gmail.com>

--- src/api-engine/api_engine/settings.py
+++ src/api-engine/api_engine/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.user",
+    "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
