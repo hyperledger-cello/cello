c1b7c5038f8052c3a8c69efc3399e5db54f02f45
dodo920306 <dodo920306@gmail.com>
2025-08-27 18:43:13 +0800

    Refactor agent into an application
    Signed-off-by: dodo920306 <dodo920306@gmail.com>

---
+++ src/api-engine/agent/serializers.py
@@ -0,0 +1,62 @@
+from rest_framework import serializers
+
+from agent.models import Agent
+from api.common.serializers import PageQuerySerializer, ListResponseSerializer
+
+IDHelpText = "ID of Agent"
+
+class AgentQuery(PageQuerySerializer, serializers.Serializer):
+    class Meta:
+        fields = ("page", "per_page")
+
+
+class AgentIDSerializer(serializers.Serializer):
+    id = serializers.UUIDField(help_text=IDHelpText)
+
+
+class AgentResponseSerializer(AgentIDSerializer, serializers.ModelSerializer):
+    organization = serializers.UUIDField(
+        help_text="Organization ID", required=True
+    )
+
+    class Meta:
+        model = Agent
+        fields = (
+            "id",
+            "name",
+            "status",
+            "created_at",
+            "type",
+            "urls",
+            "organization",
+        )
+
+
+class AgentListResponse(ListResponseSerializer):
+    data = AgentResponseSerializer(many=True, help_text="Agents data")
+
+
+class AgentCreateBody(serializers.ModelSerializer):
+    class Meta:
+        model = Agent
+        fields = (
+            "name",
+            "type",
+            "urls",
+        )
+        extra_kwargs = {
+            "type": {"required": True},
+            "urls": {"required": True},
+            "name": {"required": True},
+        }
+
+    def validate(self, data):
+        if self.context.get("request").user.organization.agent:
+            raise serializers.ValidationError("Agent Exists for the Organization")
+        return data
+
+    def create(self, validated_data):
+        validated_data["organization"] = self.context.get("request").user.organization
+        return Agent.objects.create(**validated_data)
+
+
---
+++ src/api-engine/agent/views.py
@@ -0,0 +1,74 @@
+from django.core.paginator import Paginator
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from agent.models import Agent
+from agent.serializers import AgentQuery, AgentListResponse, AgentCreateBody, AgentIDSerializer
+from api.common import ok
+from api.utils.common import with_common_response
+
+
+# Create your views here.
+class AgentViewSet(viewsets.ViewSet):
+    """Class represents agent related operations."""
+
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
+    @swagger_auto_schema(
+        query_serializer=AgentQuery(),
+        responses=with_common_response(
+            with_common_response({status.HTTP_200_OK: AgentListResponse})
+        ),
+    )
+    def list(self, request):
+        serializer = AgentQuery(data=request.GET)
+        serializer.is_valid(raise_exception=True)
+        page = serializer.validated_data.get("page")
+        per_page = serializer.validated_data.get("per_page")
+
+        p = Paginator(Agent.objects.filter(organization=request.user.organization), per_page)
+        agent_list = [
+            {
+                "id": agent.id,
+                "name": agent.name,
+                "status": agent.status,
+                "type": agent.type,
+                "urls": agent.urls,
+                "organization": (
+                    str(agent.organization.id)
+                    if agent.organization
+                    else None
+                ),
+                "created_at": agent.created_at,
+            }
+            for agent in p.page(page)
+        ]
+        response = AgentListResponse(
+            data={"data": agent_list, "total": p.count}
+        )
+        response.is_valid(raise_exception=True)
+        return Response(
+            ok(response.validated_data),
+            status=status.HTTP_200_OK,
+        )
+
+    @swagger_auto_schema(
+        request_body=AgentCreateBody,
+        responses=with_common_response(
+            {status.HTTP_201_CREATED: AgentIDSerializer}
+        ),
+    )
+    def create(self, request):
+        serializer = AgentCreateBody(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        agent = serializer.save()
+        response = AgentIDSerializer(data=agent.__dict__)
+        response.is_valid(raise_exception=True)
+        return Response(
+            ok(response.validated_data),
+            status=status.HTTP_201_CREATED,
+        )
