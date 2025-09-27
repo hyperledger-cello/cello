75ee1763809299be02727918ddd00a07681c0664
dodo920306 <dodo920306@gmail.com>
2025-09-27 23:12:06 +0800

    Finish upload chaincodes
    Signed-off-by: dodo920306 <dodo920306@gmail.com>

--- src/api-engine/channel/serializers.py
+++ src/api-engine/channel/serializers.py
@@ -6,6 +6,7 @@ from channel.models import Channel
 from channel.service import create
 from common.serializers import ListResponseSerializer
 from node.models import Node
+from node.service import get_node
 
 
 class ChannelID(serializers.Serializer):
@@ -17,7 +18,12 @@ class ChannelResponse(
 ):
     class Meta:
         model = Channel
-        fields = ("id", "name", "organizations", "created_at")
+        fields = (
+            "id",
+            "name",
+            "organizations",
+            "created_at"
+        )
 
 
 class ChannelList(ListResponseSerializer):
@@ -33,20 +39,39 @@ class ChannelCreateBody(serializers.Serializer):
         child=serializers.UUIDField(help_text="ID of Orderer Nodes")
     )
 
-    def validate(self, data):
-        peer_ids = data["peer_ids"]
-        orderer_ids = data["orderer_ids"]
+    @staticmethod
+    def validate_peer_ids(value):
+        if len(value) < 1:
+            raise serializers.ValidationError("You must specify at least one peer for a channel.")
 
-        if len(peer_ids) < 1:
-            raise serializers.ValidationError("Invalid peers")
-        if len(orderer_ids) < 1:
-            raise serializers.ValidationError("Invalid orderers")
-
-        for node in Node.objects.filter(id__in=(peer_ids + orderer_ids)):
+        for peer_id in value:
+            node = get_node(peer_id)
+            if node is None:
+                raise serializers.ValidationError("Peer {} not found.".format(peer_id))
+            if node.type != Node.Type.PEER:
+                raise serializers.ValidationError(
+                    "Node {} is not a peer but {} instead.".format(peer_id, node.type))
             if node.status != Node.Status.RUNNING:
-                raise serializers.ValidationError("Node {} is not running".format(node.name))
+                raise serializers.ValidationError("Peer {} is not running.".format(peer_id))
 
-        return data
+        return value
+
+    @staticmethod
+    def validate_orderer_ids(value):
+        if len(value) < 1:
+            raise serializers.ValidationError("You must specify at least one orderer for a channel.")
+
+        for orderer_id in value:
+            node = get_node(orderer_id)
+            if node is None:
+                raise serializers.ValidationError("Orderer {} not found.".format(orderer_id))
+            if node.type != Node.Type.PEER:
+                raise serializers.ValidationError(
+                    "Node {} is not an orderer but {} instead.".format(orderer_id, node.type))
+            if node.status != Node.Status.RUNNING:
+                raise serializers.ValidationError("Orderer {} is not running.".format(orderer_id))
+
+        return value
 
     def create(self, validated_data:Dict[str, Any]) -> ChannelID:
         return ChannelID(create(
