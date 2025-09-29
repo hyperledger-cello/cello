fddb1d1528515da54657f7863ad6ab21bc3bec98
dodo920306 <dodo920306@gmail.com>
2025-09-29 11:06:13 +0800

    Rename 'fakeAccountLogin' to 'login'
    Signed-off-by: dodo920306 <dodo920306@gmail.com>

--- src/dashboard/src/services/api.js
+++ src/dashboard/src/services/api.js
@@ -2,7 +2,7 @@
 import request from '@/utils/request';
 
 // eslint-disable-next-line import/prefer-default-export
-export async function fakeAccountLogin(params) {
+export async function login(params) {
   return request('/api/v1/login', {
     method: 'POST',
     data: params,
