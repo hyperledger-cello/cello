680a189c74c0bb39b5f8d15d45e8f5a1557793ba
dodo920306 <dodo920306@gmail.com>
2025-10-10 20:35:48 +0800

    Run prettier
    Signed-off-by: dodo920306 <dodo920306@gmail.com>

--- src/dashboard/src/components/HeaderDropdown/index.js
+++ src/dashboard/src/components/HeaderDropdown/index.js
@@ -6,8 +6,6 @@ import styles from './index.less';
 export default class HeaderDropdown extends PureComponent {
   render() {
     const { className, ...props } = this.props;
-    return (
-      <Dropdown className={classNames(styles.container, className)} {...props} />
-    );
+    return <Dropdown className={classNames(styles.container, className)} {...props} />;
   }
 }
