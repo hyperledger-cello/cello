d4420373dd7ffe6ba66103a001e1bae383be69f7
dodo920306 <dodo920306@gmail.com>
2025-11-07 11:06:44 +0800

    Add a welcome message on the home page
    Signed-off-by: dodo920306 <dodo920306@gmail.com>

--- src/dashboard/src/pages/Overview/index.js
+++ src/dashboard/src/pages/Overview/index.js
@@ -1,10 +1,24 @@
 import React, { PureComponent } from 'react';
+import { injectIntl } from 'umi';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 
 class Index extends PureComponent {
   render() {
-    return <PageHeaderWrapper>User Overview</PageHeaderWrapper>;
+    const { intl } = this.props;
+    return (
+      <PageHeaderWrapper>
+        {intl.formatMessage({
+          id: 'overview.title',
+          defaultMessage: 'User Overview',
+        })}
+        <h1 style={{ textAlign: 'center' }}>
+          {intl.formatMessage({
+            id: 'overview.welcome.message',
+            defaultMessage: 'Welcome!',
+          })}
+        </h1>
+      </PageHeaderWrapper>);
   }
 }
 
-export default Index;
+export default injectIntl(Index);
