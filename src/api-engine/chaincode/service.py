10430f01b046211398f97be4095c2c87a68de43f
dodo920306 <dodo920306@gmail.com>
2025-11-14 18:05:55 +0800

    Extract chaincode installation logic to another function
    Signed-off-by: dodo920306 <dodo920306@gmail.com>

--- src/api-engine/chaincode/service.py
+++ src/api-engine/chaincode/service.py
@@ -18,6 +18,7 @@ from user.models import UserProfile
 
 LOG = logging.getLogger(__name__)
 
+peer_command = os.path.join(FABRIC_TOOL, "peer")
 
 def create_chaincode(
         name: str,
@@ -31,7 +32,6 @@ def create_chaincode(
         description: str,
         init_required: bool = False,
         signature_policy: str = None) -> Chaincode:
-    peer_command = os.path.join(FABRIC_TOOL, "peer")
     metadata = get_metadata(package)
 
     chaincode = Chaincode(
@@ -50,57 +50,24 @@ def create_chaincode(
     chaincode.save()
     chaincode.peers.add(*peers)
 
-    peer_organization_name = organization.name.split(".", 1)[0].capitalize()
-    peer_msp = "{}MSP".format(peer_organization_name)
-    peer_root_certs = []
-    peer_addresses = []
-    peer_envs = []
-    for peer_domain_name in [get_domain_name(organization.name, Node.Type.PEER, peer.name) for peer in peers]:
-        peer_dir = get_peer_directory(organization.name, peer_domain_name)
-        peer_root_cert = os.path.join(peer_dir, "tls/ca.crt")
-        peer_address = "{}:7051".format(peer_domain_name)
-        peer_envs.append({
-            "CORE_PEER_TLS_ENABLED": "true",
-            "CORE_PEER_LOCALMSPID": peer_msp,
-            "CORE_PEER_TLS_ROOTCERT_FILE": peer_root_cert,
-            "CORE_PEER_MSPCONFIGPATH": "{}/users/Admin@{}/msp".format(
-                get_org_directory(organization.name, Node.Type.PEER),
-                organization.name
-            ),
-            "CORE_PEER_ADDRESS": peer_address,
-            "FABRIC_CFG_PATH": peer_dir,
-        })
+    peer_root_certs: list[str] = []
+    peer_addresses: list[str] = []
+    peer_envs: list[map[str, str]] = []
+    for peer in peers:
+        peer_root_cert, peer_address, peer_env = get_peer_root_cert_and_address_and_envs(organization.name, peer.name)
         peer_root_certs.append(peer_root_cert)
         peer_addresses.append(peer_address)
+        peer_envs.append(peer_env)
 
-    command = [
-        peer_command,
-        "lifecycle",
-        "chaincode",
-        "calculatepackageid",
-        chaincode.package.path
-    ]
-    LOG.info(" ".join(command))
-    with transaction.atomic():
-        chaincode.package_id = subprocess.run(
-            command,
-            env=peer_envs[0],
-            check=True,
-            capture_output=True,
-            text=True
-        ).stdout
+    set_chaincode_package_id(peer_envs[0], chaincode)
+    install_chaincode_with_envs(peer_envs, chaincode)
 
-        chaincode.save()
+    command[3] = "commit"
+    # Remove package ID
+    del command[14:16]
+    for i in range(len(peers)):
+        command.extend(["--peerAddresses", peer_addresses[i], "--tlsRootCertFiles", peer_root_certs[i]])
 
-    chaincode = Chaincode.objects.get(name=name)
-
-    command = [
-        peer_command,
-        "lifecycle",
-        "chaincode",
-        "install",
-        chaincode.package.path,
-    ]
     LOG.info(" ".join(command))
     for peer_env in peer_envs:
         subprocess.run(
@@ -109,9 +76,41 @@ def create_chaincode(
             check=True)
 
     with transaction.atomic():
-        chaincode.status = Chaincode.Status.INSTALLED
+        chaincode.status = Chaincode.Status.COMMITTED
         chaincode.save()
 
+    return chaincode
+
+def get_metadata(file) -> Optional[Dict[str, Any]]:
+    file.seek(0)
+    res = None
+    with tarfile.open(fileobj=file, mode='r:gz') as tar:
+        for member in tar.getmembers():
+            if member.name.endswith("metadata.json"):
+                res = json.loads(
+                    tar.extractfile(member)
+                    .read()
+                    .decode("utf-8")
+                )
+                break
+    file.seek(0)
+    return res
+
+def install_chaincode(organization: Organization, chaincode: Chaincode):
+    peer_envs: list[map[str, str]] = []
+    for peer in chaincode.peers:
+        peer_envs.append(get_peer_root_cert_and_address_and_envs(organization.name, peer.name)[2])
+
+    if chaincode.package_id is None:
+        set_chaincode_package_id(peer_envs[0], chaincode)
+
+    install_chaincode_with_envs(peer_envs, chaincode)
+
+def approve_chaincode(
+        peer_envs: list[dict[str, str]],
+        channel_name: str,
+        organization: Organization, 
+        chaincode: Chaincode):
     orderer_domain_name = get_domain_name(
         organization.name,
         Node.Type.ORDERER,
@@ -127,7 +126,7 @@ def create_chaincode(
         "--ordererTLSHostnameOverride",
         orderer_domain_name,
         "--channelID",
-        channel.name,
+        channel_name,
         "--name",
         chaincode.name,
         "--version",
@@ -159,12 +158,50 @@ def create_chaincode(
         chaincode.status = Chaincode.Status.APPROVED
         chaincode.save()
 
-    command[3] = "commit"
-    # Remove package ID
-    del command[14:16]
-    for i in range(len(peers)):
-        command.extend(["--peerAddresses", peer_addresses[i], "--tlsRootCertFiles", peer_root_certs[i]])
+def get_peer_root_cert_and_address_and_envs(organization_name: str, peer_name: str) -> tuple[str, str, map[str, str]]:
+    peer_domain_name: str = get_domain_name(organization_name, Node.Type.PEER, peer_name)
+    peer_dir: str = get_peer_directory(organization_name, peer_domain_name)
+    peer_root_cert: str = os.path.join(peer_dir, "tls/ca.crt")
+    peer_address: str = "{}:7051".format(peer_domain_name)
+    return peer_root_cert, peer_address, {
+        "CORE_PEER_TLS_ENABLED": "true",
+        "CORE_PEER_LOCALMSPID": "{}MSP".format(organization_name.split(".", 1)[0].capitalize()),
+        "CORE_PEER_TLS_ROOTCERT_FILE": peer_root_cert,
+        "CORE_PEER_MSPCONFIGPATH": "{}/users/Admin@{}/msp".format(
+            get_org_directory(organization_name, Node.Type.PEER),
+            organization_name
+        ),
+        "CORE_PEER_ADDRESS": peer_address,
+        "FABRIC_CFG_PATH": peer_dir,
+    }
 
+def set_chaincode_package_id(peer_env: dict[str, str], chaincode: Chaincode):
+    command: list[str] = [
+        peer_command,
+        "lifecycle",
+        "chaincode",
+        "calculatepackageid",
+        chaincode.package.path
+    ]
+    LOG.info(" ".join(command))
+    with transaction.atomic():
+        chaincode.package_id = subprocess.run(
+            command,
+            env=peer_env,
+            check=True,
+            capture_output=True,
+            text=True
+        ).stdout
+        chaincode.save()
+
+def install_chaincode_with_envs(peer_envs: list[dict[str, str]], chaincode: Chaincode):
+    command = [
+        peer_command,
+        "lifecycle",
+        "chaincode",
+        "install",
+        chaincode.package.path,
+    ]
     LOG.info(" ".join(command))
     for peer_env in peer_envs:
         subprocess.run(
@@ -173,26 +210,8 @@ def create_chaincode(
             check=True)
 
     with transaction.atomic():
-        chaincode.status = Chaincode.Status.COMMITTED
+        chaincode.status = Chaincode.Status.INSTALLED
         chaincode.save()
-
-    return chaincode
-
-
-def get_metadata(file) -> Optional[Dict[str, Any]]:
-    file.seek(0)
-    res = None
-    with tarfile.open(fileobj=file, mode='r:gz') as tar:
-        for member in tar.getmembers():
-            if member.name.endswith("metadata.json"):
-                res = json.loads(
-                    tar.extractfile(member)
-                    .read()
-                    .decode("utf-8")
-                )
-                break
-    file.seek(0)
-    return res
 
 class ChaincodeAction(Enum):
     SUBMIT = auto()
